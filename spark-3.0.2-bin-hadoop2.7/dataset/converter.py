d089077216cf3630989416e57188c681446a38c4
Luca <luca.marzari@studenti.univr.it>
2021-04-19 17:56:03 +0200

    project finished(also query 12)

--- spark-3.0.2-bin-hadoop2.7/dataset/converter.py
+++ spark-3.0.2-bin-hadoop2.7/dataset/converter.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('tag_names.dat') as dat_file, open('tag_names.csv', 'w') as csv_file:
+with open('movies.dat') as dat_file, open('movies.csv', 'w') as csv_file:
     csv_writer = csv.writer(csv_file)
 
     for line in dat_file:
